6533fab9a305ae72e61da593ae335fad1d71e435
周 奇琪 <zhouqiqi@bogon.(none)>
2011-06-23 23:04:47 +0800

    update ver

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var http = require('http');
- 
- /**
+
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world!\n");
-});
- 
-server.listen(process.env.PORT || 8001);
-*/
 var options = {
 	host: 'api.twitter.com',
 	port: 80,
@@ -17,9 +10,13 @@ var options = {
 http.get(options, function(response) {
 	console.log("Got response: " + response.statusCode);
 	response.on('data', function(chunk) {
-		response.writeHead(200, {'Content-Type': 'text/javascript'});
-		response.end(chunk);
+		res.writeHead(200, {'Content-Type': 'text/javascript'});
+		res.end(chunk);
 	})
 }).on('error', function(e) {
 	console.log("Got error: " + e.message);
 });
+});
+ 
+server.listen(process.env.PORT || 8001);
+
