2311b04cbc60c0b450eeb17b86fec73b68428783
周 奇琪 <zhouqiqi@bogon.(none)>
2011-06-23 22:51:01 +0800

    update ver

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var http = require('http');
  
+ /**
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.end("Hello world!\n");
 });
  
 server.listen(process.env.PORT || 8001);
+*/
+var options = {
+	host: 'api.twitter.com',
+	port: 80,
+	path: '/1/statuses/user_timeline.json',
+	data: {'screen_name': 'timonwang' , 'count': '100'}
+};
+http.get(options, function(response) {
+	console.log("Got response: " + response.statusCode);
+	response.on('data', function(chunk) {
+		res.writeHead(200, {'Content-Type': 'text/javascript'});
+		res.end(chunk);
+	})
+}).on('error', function(e) {
+	console.log("Got error: " + e.message);
+});
